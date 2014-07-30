0859e176e8e1e54cfa897d9502b02d48f5129bb3
Erik Nomitch <erik@nomitch.com>
2014-07-29 22:00:35 -0700

    mounts engine

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'sprockets#index'
 
+  mount Prelang::Active::Engine => '/prelang'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
