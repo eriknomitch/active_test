81f826953c04d5caad76ae40c100ce46a5ca4eab
Erik Nomitch <erik@nomitch.com>
2014-11-17 15:17:50 -0600

    create_many_sprockets

--- app/models/sprocket.rb
+++ app/models/sprocket.rb
@@ -1,3 +1,10 @@
 class Sprocket < ActiveRecord::Base
   belongs_to :user
+
+  def self.create_many_sprockets
+    100.times do |i|
+      Sprocket.create(title: "My Sprocket #{i}", description: "Description for Sprocket #{i}.")
+    end
+    true
+  end
 end
