71149a3c35aada6bf6ae7c55d4539660215ecb1f
Erik Nomitch <erik@nomitch.com>
2015-04-27 15:20:34 -0500

    devise syms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 class User < ActiveRecord::Base
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-        :recoverable, :rememberable, :trackable, :validatable
+  devise *%i(
+        database_authenticatable
+        registerable
+        recoverable
+        rememberable
+        trackable
+        validatable)
+  
+  # ----------------------------------------------
+  # RELATIONS ------------------------------------
+  # ----------------------------------------------
   has_many :sprockets
 end
